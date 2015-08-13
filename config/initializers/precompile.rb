65f8613971258802dc28e7532e965f0b1b821125
Yuri Ferreira <yuriferreiraaa@gmail.com>
2015-08-13 17:46:18 -0300

    Ajuste nos assets.

--- config/initializers/precompile.rb
+++ config/initializers/precompile.rb
@@ -1,8 +1,8 @@
-Rails.application.config.assets.precompile += %w(application.css,
-                                                  heroic-features.css,
-                                                  jquery-ui.css,
-                                                  jquery.raty.css,
-                                                  autogrow.js,
-                                                  bindWithDelay.js,
-                                                  jquery-ui.js,
-                                                  jquery.raty.js)
+#Rails.application.config.assets.precompile += %w(application.css,
+#                                                  heroic-features.css,
+#                                                  jquery-ui.css,
+#                                                  jquery.raty.css,
+#                                                  autogrow.js,
+#                                                  bindWithDelay.js,
+#                                                  jquery-ui.js,
+#                                                  jquery.raty.js)
