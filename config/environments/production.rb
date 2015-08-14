578b315d841469f3b88784c43323fdc1c6600ba1
Yuri Ferreira <yuriferreiraaa@gmail.com>
2015-08-14 13:38:25 -0300

    Configurando S3.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,11 +95,11 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :url => "s3-us-west-2.amazonaws.com",
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      :url => "s3-us-west-2.amazonaws.com"
     }
   }
 end
