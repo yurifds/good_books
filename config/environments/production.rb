f7a8ca52c402e6fb87cb399c2f074400683a1fbe
Yuri Ferreira <yuriferreiraaa@gmail.com>
2015-08-14 13:32:41 -0300

    Configurando S3.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,6 +99,7 @@ Rails.application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :url => "s3-us-west-2.amazonaws.com"
     }
   }
 end
