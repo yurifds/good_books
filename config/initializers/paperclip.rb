f8d65921673b2770fae2d5faf88176a070e2f573
Yuri Ferreira <yuriferreiraaa@gmail.com>
2015-08-14 14:00:53 -0300

    Configurando S3.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
 Rails.env.production? do
-  Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:url] = 'http://yuribooksimages.s3-website-us-west-2.amazonaws.com/'
   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 end
