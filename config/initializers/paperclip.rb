a183e238bdcb7a94e74705dcc0ce20dd25d7c16c
Yuri Ferreira <yuriferreiraaa@gmail.com>
2015-08-14 14:47:09 -0300

    Ajustando url S3.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,3 @@
 Rails.env.production? do
-  Paperclip::Attachment.default_options[:s3_host_alias] = 's3-sa-east-1.amazonaws.com'
   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 end
