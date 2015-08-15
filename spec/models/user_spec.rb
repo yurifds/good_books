468d0c5ce868a25ec5100a0387989a36e320c26c
Yuri Ferreira <yuriferreiraaa@gmail.com>
2015-08-15 16:18:02 -0300

    Improving the test coverage of model user.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe User do
       it { expect(subject).to validate_presence_of(:last_name) }
     end
   end
+
+  describe "#rating_by_user " do
+    let(:current_user) { FactoryGirl.create(:user_test) }
+    let(:book) { FactoryGirl.create(:book, user: current_user) }
+
+    it "when user classified the book" do
+      FactoryGirl.create(:rating, user: current_user, book: book)
+      expect(current_user.rating_by_user(book)).to eql(3)
+    end
+
+    it "when the user doesn't classify the book" do
+      expect(current_user.rating_by_user(book)).to eql(0)
+    end
+  end
 end
