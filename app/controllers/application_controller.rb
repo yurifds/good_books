b4c963478b98f7d04ed3be65ed9ec3ba5eff48f6
Yuri Ferreira <yuriferreiraaa@gmail.com>
2015-08-28 18:54:33 -0300

    adjustment in code formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :last_name, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name,:last_name, :email, :password, :password_confirmation, :current_password) }
   end
-  
+
   def set_search
     @search = Book.search(params[:q])
   end
+  
 end
